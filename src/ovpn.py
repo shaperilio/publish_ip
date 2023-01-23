d829d84e53ba37ea05d6f63187be9fd19273833e
Emilio Graff <1@emil.io>
2023-01-23 09:59:32 -0800

    Only modify file if IP address changed.

--- src/ovpn.py
+++ src/ovpn.py
@@ -5,10 +5,10 @@ class ConfigParsingError(Exception):
     pass
 
 
-def replace_ip(config_file: str, new_ip: str) -> None:
+def check_and_replace_ip(config_file: str, new_ip: str) -> bool:
     """
     Replaces the IP address in the OpenVPN configuration file at `config_file` with the value in
-    `new_ip`. The file is overwritten without backup.
+    `new_ip`, if they don't match. The file is overwritten without backup.
 
     Raises
     ------
@@ -21,6 +21,11 @@ def replace_ip(config_file: str, new_ip: str) -> None:
         Path to the OpenVPN configuration file
     new_ip : str
         The IP address to put in said file.
+
+    Returns
+    -------
+    bool:
+        True if the file was updated; False if `new_ip` matches the IP address in the file.
     """
 
     with open(config_file, 'r') as f:
@@ -30,6 +35,11 @@ def replace_ip(config_file: str, new_ip: str) -> None:
     if match is None:
         raise ConfigParsingError('Cannot find a line containing "remote <IPv4 address>".')
     remote_line = match[0]
-    new_config = config.replace(remote_line, f'remote {new_ip}')
-    with open(config_file, 'w') as f:
-        f.write(new_config)
+    ip_address = remote_line.split(' ')[1]
+    if ip_address == new_ip:
+        return False
+    else:
+        new_config = config.replace(remote_line, f'remote {new_ip}')
+        with open(config_file, 'w') as f:
+            f.write(new_config)
+        return True
--- src/runner.py
+++ src/runner.py
@@ -2,7 +2,7 @@ import shutil
 import time
 
 from ip_getters import get_ip_ifconfig_me
-from ovpn import replace_ip
+from ovpn import check_and_replace_ip
 from dropbox import get_dropbox_path_business
 from output import log, seconds2pretty
 
@@ -23,22 +23,17 @@ if __name__ == '__main__':
     config_file = args.source_ovpn_file[0]
     dropbox_loc = args.dest_ovpn_file[0]
     interval_sec = float(args.interval_minutes) * 60
-    last_ip = None
     while True:
         try:
             my_ip = get_ip_ifconfig_me()
-            if last_ip is None:
-                replace_ip(config_file, my_ip)
-
+            if check_and_replace_ip(config_file, my_ip):
                 dropbox = get_dropbox_path_business()
                 pub_path = f'{dropbox}/{dropbox_loc}'
                 shutil.copyfile(config_file, pub_path)
-
-                last_ip = my_ip
                 log(f'IP address updated to {my_ip}; file saved at {pub_path}.')
-
-            elif my_ip == last_ip:
+            else:
                 log(f'IP address is still {my_ip}; no update necessary.')
+
         except Exception as e:
             log(f'Exception encountered:\n\n{e}\n\nRetrying in 1 minute.')
             time.sleep(60)
